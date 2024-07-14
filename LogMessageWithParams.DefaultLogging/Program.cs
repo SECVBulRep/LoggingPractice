12529b1ac533df16b79661beb936aa8d40654584
SECVBulRep <bfarukshin@gmail.com>
2024-07-14 20:31:15 +0300

    * добавил консольное приложение

---
+++ LogMessageWithParams.DefaultLogging/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+using var  _loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole().SetMinimumLevel(LogLevel.Information);
+});
+
+ILogger logger = new Logger<Program>(_loggerFactory);
+
+logger.LogInformation("Random number {RandomNumber}",Random.Shared.Next());
+
+Console.ReadKey();
