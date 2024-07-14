c00c393e5529419a620b74e61fef1b5e1a3462e7
SECVBulRep <bfarukshin@gmail.com>
2024-07-14 20:34:17 +0300

    Покажи сколько было аллоцировано

--- LogMessageWithParams.DefaultLogging/Program.cs
+++ LogMessageWithParams.DefaultLogging/Program.cs
@@ -1,13 +1,12 @@
-﻿
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
-using var  _loggerFactory = LoggerFactory.Create(builder =>
-{
-    builder.AddConsole().SetMinimumLevel(LogLevel.Information);
-});
+using var _loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole().SetMinimumLevel(LogLevel.Warning); });
 
 ILogger logger = new Logger<Program>(_loggerFactory);
 
-logger.LogInformation("Random number {RandomNumber}",Random.Shared.Next());
+for (int i = 0; i < 70_000_000; i++)
+{
+    logger.LogInformation("Random number {RandomNumber}", Random.Shared.Next());
+}
 
-Console.ReadKey();
+//Console.ReadKey();
