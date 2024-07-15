75368b7b3e57c6f4593ece3388c80fb243d954bc
bulat <farukshin@molot.ru>
2024-07-15 19:26:10 +0300

    * Решение через адаптер

--- LoggingPracticeBenchmark/Benchmarkie.cs
+++ LoggingPracticeBenchmark/Benchmarkie.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Logging;
 
 namespace LoggingPracticeBenchmark;
 
-
 //[SimpleJob(RunStrategy.Monitoring)]
 [MemoryDiagnoser]
 public class Benchmarkie
@@ -20,10 +19,12 @@ public class Benchmarkie
     });
 
     private readonly ILogger<Benchmarkie> _logger;
+    private readonly ILoggerAdapter<Benchmarkie> _loggerAdapter;
 
     public Benchmarkie()
     {
         _logger = new Logger<Benchmarkie>(_loggerFactory);
+        _loggerAdapter = new LoggerAdapter<Benchmarkie>(_logger);
     }
 
     [Benchmark(Baseline = true)]
@@ -38,19 +39,24 @@ public class Benchmarkie
         if (_logger.IsEnabled(LogLevel.Information))
             _logger.LogInformation(LogMessage);
     }
-    
-    
+
+
     [Benchmark]
     public void LogWithOut_If_WithParams()
     {
-        _logger.LogInformation(LogMessageWithParams, 1,2,3);
+        _logger.LogInformation(LogMessageWithParams, 1, 2, 3);
     }
 
     [Benchmark]
     public void LogWith_If_WithParams()
     {
         if (_logger.IsEnabled(LogLevel.Information))
-            _logger.LogInformation(LogMessageWithParams,1,2,3);
+            _logger.LogInformation(LogMessageWithParams, 1, 2, 3);
     }
-    
+
+    [Benchmark]
+    public void LogAdapterWith_If_WithParams()
+    {
+        _loggerAdapter.LogInformtion(LogMessageWithParams, 1, 2, 3);
+    }
 }
---
+++ LoggingPracticeBenchmark/LoggerAdapter.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Logging;
+
+namespace LoggingPracticeBenchmark;
+
+public class LoggerAdapter<T> : ILoggerAdapter<T>
+{
+
+    private ILogger<T> _logger;
+
+    public LoggerAdapter(ILogger<T> logger)
+    {
+        _logger = logger;
+    }
+    
+    public void LogInformtion(string message)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message);
+        }
+    }
+
+    public void LogInformtion<T0>(string message, T0 arg0)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message,arg0);
+        }
+    }
+
+    public void LogInformtion<T0, T1>(string message, T0 arg0, T1 arg1)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message,arg0,arg1);
+        }
+    }
+
+    public void LogInformtion<T0, T1, T2>(string message, T0 arg0, T1 arg1, T2 arg2)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message,arg0,arg1,arg2);
+        }
+    }
+}
+
+public interface ILoggerAdapter<T>
+{
+    void LogInformtion(string message);
+
+    void LogInformtion<T0>(string message, T0 arg0);
+
+    void LogInformtion<T0, T1>(string message, T0 arg0, T1 arg1);
+
+    void LogInformtion<T0, T1, T2>(string message, T0 arg0, T1 arg1, T2 arg2);
+}
